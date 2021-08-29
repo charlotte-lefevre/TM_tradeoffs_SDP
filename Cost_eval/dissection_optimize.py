fc150ef88e8867e71752f784322657b08c2fbbb0
Charlotte Lefevre <charlotte.lefevre@etu.univ-grenoble-alpes.fr>
2021-08-29 14:27:42 +0200

    working dissection_optimize + Readme update

--- Cost_eval/dissection_optimize.py
+++ Cost_eval/dissection_optimize.py
@@ -12,16 +12,21 @@ from Wave_param import * # R and W
 # Note that this function does not include a precomputation phase, which consits in "looking roughly at the best Re's candidates". 
 # The code here is just a precise way to get the best possible points
 def compute_best_complexity_dissection(r=120, final_treatement=True): 
-    M_tot = [];T_tot = [];final_Res = []
-    Res = np.linspace(0.001,0.034, 40)
-    for Re in Res:
-        (M,T,col) = dissection(r, R, Re,W, nbMem=150)
-        for i in range(0, len(M)):
-            M_tot = np.append(M_tot, M[i])
-            T_tot = np.append(T_tot, T[i])
-            final_Res = np.append(final_Res, Re)
-                
-        if (final_treatement):
-            M_tot, T_tot, final_Res = clean_array(M_tot,T_tot, final_Res)
+    M_tot = [];T_tot = [];cols_tot = []
 
-    return(M_tot, T_tot,final_Res )
+    #Res = np.append(np.linspace(0.001, 0.025, 25), np.linspace(0.025, 0.034, 10))
+    Res = np.linspace(0.001,0.034, 45)
+
+
+    for Re in Res:
+        (M,T,_) = dissection(r, R, Re,W, nbMem=100)
+        #plt.scatter(M,T, label="l" + r'$ \approx$' + str(round(Re,4)) + "n", s=20)
+        #plt.scatter(M,T, label="l = " + str(round(Re,4)) + "n", s=20)
+        #plt.scatter(M,T, s=20)
+        M_tot = np.append(M_tot, M)
+        T_tot = np.append(T_tot, T)
+    if final_treatement:
+        M_tot, T_tot = clean_array(M_tot,T_tot, [])
+
+    return M_tot,T_tot
+
--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Tools useful for ternary syndrome decoding with large weight with Wave's paramet
 This repository is composed of two main parts:
 * **Cost_eval** aims at computing time and memory cost for the different algorithms
 * **Implem_34_dissec** is a PoC of 4 dissection organized in a tree structure with 3 levels
+To have a global overview of the projects, please use the doc/ folder inside each one of the projects, it will be much easier to read the latex formulas.
 
 # Requirements
 * Sagemath (9.2)
